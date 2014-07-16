e067120d1b99ab6a5ee2dc6d127063b9d6f668a6
triffe <tim.riffe@gmail.com>
2014-07-16 13:06:20 +0200

    now long format taken as input, and wide processing code as been removed. some extra steps are taken to recode common factors into binary.

--- ThanoEmpirical/R/PreProcessing.R
+++ ThanoEmpirical/R/PreProcessing.R
@@ -1,5 +1,10 @@
 # install.packages("lubridate")
-
+convertYN <- function(x){
+    xx                  <- rep(NA, length(x))
+    xx[grepl("yes", x)] <- 1
+    xx[grepl("no", x)]  <- 0
+    invisible(xx)
+}
 convertDates <- function(Dat){
     # can't be done with apply because we can't have Date class matrices...
     DateInd       <- grep(pattern="_dt",colnames(Dat))
@@ -15,66 +20,40 @@ getThanoAge <- function(Date, DeathDate){
     out
 }
 # converts to long format, assumes thano age columns already appended:
-getLong <- function(Dat, 
-        vars_ta = c("int_dt","adl3_","adl5_","iadl3_","iadl5_",
-                "cesd","cen","div","mar","lim_work",
-                "srh","bmi","back","ta_","p_wt"), 
-        vars = c("id","sex","wt","d_yr")){
-    # cut down in case some columns not available
-    vars_ta <- vars_ta[colSums(sapply(vars_ta,grepl,colnames(Dat))) > 0]
-    Vars_ta <- outer(vars_ta,1:10,paste0)
-    Dlong   <- list()
-    for (i in 1:ncol(Vars_ta)){ # i <- 1
-        cols           <- c(vars, Vars_ta[,i])
-        colsExtra      <- cols[!cols %in% colnames(Dat)]
-        if (length(colsExtra) > 0){
-            for (z in colsExtra){
-                Dat[[z]] <- NA
-            }
-        }
-        
-        Dati           <- Dat[, cols]
-        colnames(Dati) <- c(vars, gsub("\\_*\\d*$", "", Vars_ta[,i]))
-        Dlong[[i]]     <- Dati
-    } 
-    Dlong         <- do.call(rbind, Dlong)    
-    invisible(Dlong)
-}
 
 # setwd needs to change to wherever your parent folder is, assuming the .gz file
 # is in a 'Data' folder, and actually has the .gz suffix on it :-)
 
 setwd("/home/triffe/git/ThanoEmpirical/ThanoEmpirical")
-#Dat         <- local(get(load("Data/thanos_wide_v1_0.gz")))
-
-# v1_1 is the same, but includes more variables, andhas person-weights
-#Dat         <- local(get(load("Data/thanos_wide_v1_1.gz")))
 
 # v2_2 has even more variables:
-Dat         <- local(get(load("Data/thanos_wide_v2_1.gz")))
+Dat         <- local(get(load("Data/thanos_long_v2_0.gz")))
+
+# change all factors to character (to be later recoded in some instances)
+Dat[sapply(Dat, is.factor)] <- lapply(Dat[sapply(Dat, is.factor)], as.character)
+
 # make sex column easier to use:
 Dat$sex     <- ifelse(as.character(Dat$sex) == "1.male","m","f")
+
 # reduce to deceased-only
 Dat         <- Dat[Dat$dead == 1, ]
+
+# remove missed interviews
+Dat         <- Dat[!is.na(Dat$intv_dt), ]
+
 # convert dates to native R format
 Dat         <- convertDates(Dat)
 
-# locate Date columns for calculation of thanatological age:
-DInd        <- grep(colnames(Dat),pattern="d_dt")
-DateInd     <- grep(colnames(Dat),pattern="int_dt")
-TaColnames  <- paste0("ta_",gsub(pattern = "int_dt",replacement = "",colnames(Dat)[DateInd]))
-# produce new columns for thano age at each survey wave
-for (i in 1:length(DateInd)){
-    Dat[[TaColnames[i]]] <- getThanoAge(Dat[,DateInd[i]],Dat[,DInd])
-}
-# now move to long format
-Dat         <- getLong(Dat)
+# calculate thanatological age
+Dat$ta <- getThanoAge(Dat$intv_dt, Dat$d_dt)
 
-# if a person was absent at a given wave, their ta is NA
-Dat         <- Dat[!is.na(Dat$ta), ]
+# convert yes/no responses to 1,0
+YNcols <- apply(Dat, 2, function(x){
+        xx <- unique(x)
+        length(xx) <= 3 & any(grepl("yes",xx))
+        })
+Dat[YNcols] <- lapply(Dat[YNcols], convertYN)
 
-# dim(Dat)
 # save out, so this doesn't need to be re-run every time
 save(Dat,file = "Data/Data_long.Rdata")
-
 
