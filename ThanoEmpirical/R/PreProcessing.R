75da46cd8356e0e8b3724f9802a0005ae0bbf4c5
triffe <triffe@demog.berkeley.edu>
2014-10-07 17:51:50 -0700

    squeezed responses between 0 and 1, where 1 is worst and 0 is best. There are probably more variables that need this treatment, and others that aren't eligible.

--- ThanoEmpirical/R/PreProcessing.R
+++ ThanoEmpirical/R/PreProcessing.R
@@ -153,7 +153,7 @@ Dat$ulc       <- NULL
 Dat$lt_freq   <- NULL
 Dat$mod_freq  <- NULL
 Dat$vig_freq  <- NULL
-
+Dat$c86b      <- NULL # only in a couple waves
 # recode medical expenditure to actual values:
 
 #1=0 to $1,000                 500
@@ -169,45 +169,73 @@ Dat$vig_freq  <- NULL
 #11=$500,000+              1000000
 
 rec.vec <- c(500,1000,2500,5000,15000,25000,62500,100000,300000,500000,1000000)
-names(rec.vec) <- 1:11
-Dat$med_exp    <- rec.vec[as.character(Dat$med_exp)]
-Dat$med_explog <- log(Dat$med_exp )
+names(rec.vec)      <- 1:11
+Dat$med_exp         <- rec.vec[as.character(Dat$med_exp)]
+Dat$med_explog      <- log(Dat$med_exp )
 # recode self reported health 1 = excellent - 5 = poor
-srhrec <- c(0:4,NA)
-names(srhrec) <- sort(unique(Dat$srh))
-Dat$srh <- srhrec[Dat$srh] / 4 # now all between 0 and 1. 1 worst.
-names(srhrec) <- sort(unique(Dat$srm))
-Dat$srm <- srhrec[Dat$srm] / 4 
+srhrec              <- c(0:4,NA)
+names(srhrec)       <- sort(unique(Dat$srh))
+Dat$srh             <- srhrec[Dat$srh] / 4 # now all between 0 and 1. 1 worst.
+names(srhrec)       <- sort(unique(Dat$srm))
+Dat$srm             <- srhrec[Dat$srm] / 4 
 
 # same, worse, better recode:  (1 bad, 0 good)
-pastmem <- c(0:2,NA)
-names(pastmem) <- sort(unique(Dat$pastmem))
-Dat$pastmem <- pastmem[Dat$pastmem] / 2
+pastmem             <- c(0:2,NA)
+names(pastmem)      <- sort(unique(Dat$pastmem))
+Dat$pastmem         <- pastmem[Dat$pastmem] / 2
 
 # do cesd questions (1 bad, 0 good)
-Dat[cesdquestions] <- colnames(Dat)[grepl("cesd", colnames(Dat))]
-cesdquestions      <- cesdquestions[cesdquestions != "cesd"]
-Dat[cesdquestions] <- lapply(Dat[cesdquestions],convertCESD)
+cesdquestions       <- colnames(Dat)[grepl("cesd", colnames(Dat))]
+cesdquestions       <- cesdquestions[cesdquestions != "cesd"]
+Dat[cesdquestions]  <- lapply(Dat[cesdquestions],convertCESD)
 
-# Edit: now removed because of waves gap (large)
-# light, moderate and vigorous physical activity was coded differently in wave 1 vs other waves...
-# now 0 is no activity, and
-#activity <- c("lt_freq","mod_freq","vig_freq")
-#
-#Dat[activity] <- lapply(Dat[activity], function(x,wavei){
-#   
-#    x[wavei & x == 4 & !is.na(x)]                 <- 4.5
-#    x[wavei & x == 3 & !is.na(x)]                 <- 4
-#    x[wavei & x == 2 & !is.na(x)]                 <- 3
-#    x[wavei & x == 1 & !is.na(x)]                 <- 1.5
-#    x[!wavei & x == "1.every day" & !is.na(x)]    <- 1
-#    x[!wavei & x == "2.> 1 per week" & !is.na(x)] <- 2
-#    x[!wavei & x == "3.1 per week" & !is.na(x)]   <- 3
-#    x[!wavei & x == "4.l-3 per mon" & !is.na(x)]  <- 4
-#    x[!wavei & x == "5.never" & !is.na(x)]        <- 5
-#    as.numeric(x)
-#  },wavei=wavei)
 
+# create a single Total Word Recall variables, twr
+#"tr20w"(waves(2-10),"tr40w" (waves1-2)
+# i.e. 1 is the worst recall, and 0 is the best recall
+Dat$tr20w                   <- 1 - Dat$tr20w / 20
+Dat$tr40w                   <- 1 - Dat$tr40w / 40
+
+NAind                       <- is.na(Dat$tr20w) & is.na(Dat$tr40w)
+BothInd                     <- !is.na(Dat$tr20w) & !is.na(Dat$tr40w)
+Dat$tr20w[is.na(Dat$tr20w)] <- 0
+Dat$tr40w[is.na(Dat$tr40w)] <- 0
+sum(BothInd) == 0 # (otherwise we'd need to divide these by two after adding)
+Dat$twr                     <- Dat$tr20w + Dat$tr40w
+Dat$twr[NAind]              <- NA
+
+# vocab: 1 worst 0 best
+Dat$vocab <- 1 - Dat$vocab / 10
+
+# total mental: 1 worst, 0 best
+Dat$tm    <- 1 - Dat$tm / 15
+
+# delayed word recall
+Dat$dr20w                   <- 1 - Dat$dr20w / 20
+Dat$dr10w                   <- 1 - Dat$dr10w / 10
+
+NAind                       <- is.na(Dat$dr20w) & is.na(Dat$dr10w)
+BothInd                     <- !is.na(Dat$dr20w) & !is.na(Dat$dr10w)
+Dat$dr20w[is.na(Dat$dr20w)] <- 0
+Dat$dr10w[is.na(Dat$dr10w)] <- 0
+sum(BothInd) == 0 # (otherwise we'd need to divide these by two after adding)
+Dat$dwr                    <- Dat$dr20w + Dat$dr10w
+Dat$dwr[NAind]              <- NA
+
+# immediate word recall
+Dat$ir20w                   <- 1 - Dat$ir20w / 20
+Dat$ir10w                   <- 1 - Dat$ir10w / 10
+
+NAind                       <- is.na(Dat$ir20w) & is.na(Dat$ir10w)
+BothInd                     <- !is.na(Dat$ir20w) & !is.na(Dat$ir10w)
+Dat$ir20w[is.na(Dat$ir20w)] <- 0
+Dat$ir10w[is.na(Dat$ir10w)] <- 0
+sum(BothInd) == 0 # (otherwise we'd need to divide these by two after adding)
+Dat$iwr                     <- Dat$ir20w + Dat$ir10w
+Dat$iwr[NAind]              <- NA
+
+
+# vocab
 # save out, so this doesn't need to be re-run every time
 save(Dat,file = "Data/Data_long.Rdata")
 
