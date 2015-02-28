45df0dee4b13ed90d232a91c4c48d0ba0b356a7e
triffe <triffe@demog.berkeley.edu>
2015-02-27 21:26:04 -0800

    added comment only

--- ThanoEmpirical/R/CreateMatrices.R
+++ ThanoEmpirical/R/CreateMatrices.R
@@ -91,7 +91,7 @@ varnames <- c("adl3_",
   "srm", "pastmem", "ss", "c20b", "name_mo", 
   "name_dmo", "name_yr", "name_dwk", "name_sci", "name_cac", "name_pres", 
   "name_vp", "vocab", "tm", "med_exp", "dwr","twr","iwr",
-  "iadl_calc", "mprob", "mprobev", "med_explog")
+  "iadl_calc", "mprob", "mprobev", "med_explog") # this is bigger than the final list...
 
 varnames <- varnames[varnames %in% colnames(Dat)]
 
