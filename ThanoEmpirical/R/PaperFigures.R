1d3ee65cd68f8cac05f6bf267c4501a1960b72b7
Tim Riffe <tim.riffe@gmail.com>
2016-09-28 13:38:58 +0200

    cleaned up paper figures code

--- ThanoEmpirical/R/PaperFigures.R
+++ ThanoEmpirical/R/PaperFigures.R
@@ -19,6 +19,7 @@ library(lattice)
 # this script contains just the figures used in the final submission to VYPR.
 # many other figures are produced here and there in other R scripts, but are not included.
 
+
 # ---------------------------------------------
 # Figure 1
 #
@@ -70,21 +71,19 @@ text(75,60,"(C) area studied for\n1915-1919 birth cohort", cex = .8, pos = 4)
 segments(90,55,96-13,13,col=brewer.pal(8,"Greens")[8],lty=2)
 dev.off()
 # end figure 1
-# ---------------------------------------------
-
-
-
-# figure for a mailmail
-
-
-# use LoessQuinquennial
-
 
 # ----------------------------
-# thanatological
+# Surface figures:
+
+# load in smoothed surfaces
+# object created in R/loessSmoothing.R
+Results <- local(get(load("Data/LoessQuinquenal.Rdata")))
+
+# ----------------------------
+# T: thanatological age
 # chrono: psych
-grabber <- paste0("psych","_",.7)
-Surf <- Results[[grabber]][["Male"]]$Surf[,,as.character(1915)]
+grabber <- paste0("psych", "_", .7)
+Surf <- Results[[grabber]][["Male"]]$Surf[, , as.character(1915)]
 # 
 pdf("Figures/Surf_Male_psych.pdf", width = 10, height = 6)
 #dev.new(width = 10, height = 6)
@@ -99,9 +98,9 @@ dev.off()
 
 
 # ----------------------------
-# chronological
-grabber        <- paste0("back","_",.7)
-Surf           <- Results[[grabber]][["Female"]]$Surf[,,as.character(1915)]
+# A: chronological age
+grabber        <- paste0("back", "_", .7)
+Surf           <- Results[[grabber]][["Female"]]$Surf[, , as.character(1915)]
 Surf[Surf < 0] <- 0
 pdf("Figures/Surf_Female_back.pdf", width = 10, height = 6)
 #dev.new(width = 10, height = 6)
@@ -115,9 +114,9 @@ SurfMap(Surf,
 dev.off()
 
 # ----------------------------
-# lifespan:
-grabber <- paste0("smoke_ev","_",.7)
-Surf    <- Results[[grabber]][["Female"]]$Surf[,,as.character(1915)]
+# L: lifespan:
+grabber <- paste0("smoke_ev", "_", .7)
+Surf    <- Results[[grabber]][["Female"]]$Surf[, , as.character(1915)]
 
 pdf("Figures/Surf_Female_smoke_ev.pdf", width = 10, height = 6)
 #dev.new(width = 10, height = 6)
@@ -131,9 +130,9 @@ SurfMap(Surf,
 dev.off()
 
 #------------------------------------------------
-# function of both:
-grabber <- paste0("bp","_",.7)
-Surf    <- Results[[grabber]][["Male"]]$Surf[,,as.character(1915)]
+# M: function of both
+grabber <- paste0("bp", "_", .7)
+Surf    <- Results[[grabber]][["Male"]]$Surf[, , as.character(1915)]
 
 pdf("Figures/Surf_Male_bp.pdf", width = 10, height = 6)
 #dev.new(width = 10, height = 6)
@@ -153,17 +152,33 @@ dev.off()
 # It shows an increasing pattern over age, due to
 # an interaction with lifespan.
 #------------------------------------------------
-mod     <- stats::loess('psych~cafloor+Coh5',
+# this code copied from
+Dat      <- local(get(load("Data/Data_long.Rdata")))
+# no ages under 65
+Dat      <- Dat[Dat$age >= 65, ]
+# birth year must be known
+Dat      <- Dat[!is.na(Dat$b_yr), ]
+# group to quinquennial
+Dat$Coh5 <- Dat$b_yr -  Dat$b_yr %% 5 
+# Coh5keep are cohorts kept for fitting
+Coh5keep <- c(1900, 1905, 1910, 1915, 1920, 1925, 1930)
+# Cog5 are cohorts for predicting
+Coh5     <- c(1905, 1910, 1915, 1920, 1925) # i.e. we use the preceding and subsequent cohorts for help fitting
+# select only cohorts used for fitting
+Dat      <- Dat[Dat$Coh5 %in% Coh5keep, ]
+# fit age-only within-cohort loess model
+mod      <- stats::loess('psych~cafloor+Coh5',
 		                data = Dat[Dat$sex == "m",],
 						weight = p_wt2)
-m1915se <- predict(mod,
+# predict for 1915 cohort, keep intervals
+m1915se  <- predict(mod,
 		           newdata = data.frame(cafloor = 72:95, Coh5 = 1915),
 		           se = TRUE)
 
-
+# produce figure 3
 pdf("Figures/MalePsychChrono.pdf",height = 4.5,width=4.5)
 par(mai = c(1.2,1,.5,.5))
-plot(72:95,m1915se$fit, type = 'l', xaxs = "i", yaxs="i", ylim=c(0,.25),
+plot(72:95, m1915se$fit, type = 'l', xaxs = "i", yaxs="i", ylim=c(0,.25),
   xlab = "chronological", ylab = "prevalence of psych problems",
   panel.first = list(
     rect(72,0,95,.25,col=gray(.9),border=NA),
@@ -177,6 +192,7 @@ dev.off()
 # --------------------------------
 # correlation histograms:
 
+# File produced in Correlations.R
 #
 Results_r  <- local(get(load("Data/Correlations.Rdata")))
 Hist7      <- Results_r[Results_r$span == "0.7" & Results_r$Cohort == 1915, ]
@@ -214,5 +230,5 @@ histogram(~R | Dim,
 		ylim = c(0, 47))
 dev.off()
 
-
+# ----------------------------
 # end
